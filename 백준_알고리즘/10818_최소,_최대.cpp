0b2f8c733d292bcb84c669c3473102ae96936525
Sunghee <630sunghee@gmail.com>
2020-03-26 16:38:05 +0900

    10818 - 최소, 최대 :book:

---
+++ 백준_알고리즘/10818_최소,_최대.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int num[n];
+    for(int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+
+    sort(num, num + n);
+
+    cout << num[0] << " " << num[n - 1] << endl;
+}
